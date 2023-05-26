4dfb14c7c7a2aa6a0251e8620716950fd9b5c925
Olzhas Mukayev <65302894+olzhasmukayev@users.noreply.github.com>
2023-05-26 10:38:02 +0600

    client

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -8,25 +8,35 @@ const LoginForm: React.FC = () => {
   const dispatch = useAppDispatch();
 
   return (
-    <div>
-      <input
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-        type="text"
-        placeholder="email"
-      />
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-        type="password"
-        placeholder="password"
-      />
-      <button onClick={() => dispatch(login({ email, password }))}>
-        Логин
-      </button>
-      <button onClick={() => dispatch(registration({ email, password }))}>
-        Регистрация
-      </button>
+    <div className="flex flex-col justify-center w-full h-screen items-center px-5 py-4">
+      <div className="flex flex-col h-full items-center justify-center gap-2">
+        <input
+          className="border-[1px]"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="text"
+          placeholder="email"
+        />
+        <input
+          className="border-[1px]"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          placeholder="password"
+        />
+        <button
+          className="border-[1px]"
+          onClick={() => dispatch(login({ email, password }))}
+        >
+          Логин
+        </button>
+        <button
+          className="border-[1px]"
+          onClick={() => dispatch(registration({ email, password }))}
+        >
+          Регистрация
+        </button>
+      </div>
     </div>
   );
 };
